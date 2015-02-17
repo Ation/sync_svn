c90a735eab69ca4b91afbccc7579848500bc78be
Ation <gation@gmail.com>
2015-02-17 20:15:51 +0200

    Update script

--- sync_svn.ps1
+++ sync_svn.ps1
@@ -1,122 +1,189 @@
-if (( $args.Count -lt 3) -or ($args.Count -gt 4)) {
-    write "Usage: <report file> <local path> <remote path> [<settings_file>]"
-    return
-}
 
-#check if svn acessible
-try {
-    $dummy = svn help
-} 
-catch
+# add default values
+param( [string]$local_src_path = "", [string]$remote_path="", [string]$report_file="" )
+
+Function CopyRequired ( $path )
 {
-    write "ERROR: SVN not found"
-    return
+    $accepted_extensions = @(".cxx", ".hxx", ".c", ".cpp", ".h", ".hpp", "Makefile", ".sh", ".py", ".txt", ".thor")
+
+    if ( test-path $path -PathType Leaf) {
+        foreach ($extension in $accepted_extensions) {
+            if ($path.EndsWith( $extension) ) {
+                return $True
+            }
+        }
+    } else {
+        return (($path -ne "ThorServers") -and (!$path.Contains("nbproject") )  -and (!$path.EndsWith("build\config") )  -and (!$path.EndsWith("build\.metadata") ) -and (!$path.EndsWith("build\var") ) )
+    }
+
+    return $False
 }
 
-. "$PSScriptRoot\report_tools.ps1"
-. "$PSScriptRoot\settings_tools.ps1"
-
-#########################################################
-#                    Settings
-#########################################################
-
-$report_file = $args[0]
-
-$local_src_path = $args[1]
-$remote_src_path = $args[2]
-
-######################################################### EO settings
-
-Function GetLocalPath($file_path)
+Function WriteFilesToXMLWriter( $writer, $elementName, $files)
 {
-    return join-path $local_src_path $file_path
+    $writer.WriteStartElement($elementName)
+    foreach ( $path in $files)
+    {
+        $writer.WriteStartElement("file")
+        $writer.WriteAttributeString("path", $path)
+        $writer.WriteEndElement()
+    }
+    $writer.WriteEndElement()
 }
-
-Function GetPathOnRemote($file_path)
-{
-    return join-path $remote_src_path $file_path
-}
-
-##################################### main ####################################
 
 cd $local_src_path
 
-############################## load sync settings #############################
+$old_copied_files = new-object System.Collections.ArrayList
+$old_deleted_files = new-object System.Collections.ArrayList
+$old_unversioned_files = new-object System.Collections.ArrayList
 
-if ($args.Count -eq 4) {
-    $sync_settings = LoadSettingsFromFile( $args[3])
-} else {
-    $sync_settings = CreateEmptySettings
-}
-
-########################## read previous operations ###########################
-
-$old_report = LoadReportFromFile( $report_file)
-if ( Test-Path $report_file ) {
+# read old status
+if (Test-Path $report_file) {
+    $content = [xml](get-content $report_file)
     Remove-Item $report_file
-}
 
-################################# get status ##################################
+    if ( $content.Files.ToCopy.file -ne $null) {
+        foreach ($file_node in $content.Files.ToCopy.file) {
+            $count = $old_copied_files.Add( $file_node.GetAttribute("path") )
+        }
+    }
+
+    if ( $content.Files.ToDelete.file -ne $null) {
+        foreach ($file_node in $content.Files.ToDelete.file) {
+            $count = $old_deleted_files.Add( $file_node.GetAttribute("path") )
+        }
+    }
+
+    if ( $content.Files.Unversioned.file -ne $null) {
+        foreach ($file_node in $content.Files.Unversioned.file) {
+            $count = $old_unversioned_files.Add( $file_node.GetAttribute("path") )
+        }
+    }
+}
 
 $status = [xml](svn st --xml)
 
-$report = GetSVNReport $status $sync_settings
+$copy_files = new-object System.Collections.ArrayList
+$delete_files = new-object System.Collections.ArrayList
+$unversioned_files = new-object System.Collections.ArrayList
 
-########################### save current operations ###########################
-
-SaveReport $report_file $report
-
-############################### merge operations ##############################
-# to determine if we need to do restore for something
-
-MergeReports $report $old_report
-
-########################### update remote repository ##########################
-
-if (! $report.IsEmpty)
+#create new actions list
+foreach ($entry in $status.status.target.entry)
 {
-    # process directories first
-    foreach ($directory in $report.DirectoryToCopy + $report.DirectoryUnversioned) {
-        $local_directory_path = GetLocalPath( $directory ) + "\*"
-        $remote_directory_path = GetPathOnRemote( $directory )
+    $file_path = $entry.path
+    $node = $entry.SelectSingleNode("wc-status")
+    $file_status = $node.GetAttribute("item")
 
-        write "Copy directory: $remote_directory_path"
-        if ( test-path $remote_directory_path ) {
-            # if directory should be copied - old version should be removed if exists
-            Remove-Item $remote_directory_path -recurse -force
-        }
-
-        Copy-Item -force -recurse $local_directory_path $remote_directory_path
+    if ( ($file_status -eq "missing") -or ($file_status -eq "deleted") )
+    {
+        $dummy = $delete_files.Add($file_path)
     }
-
-    foreach ( $directory in $report.DirectoryToDelete ) {
-        $remote_directory_path = GetPathOnRemote( $directory )        
-
-        write "Delete directory: $remote_directory_path"
-        if (test-path $remote_directory_path) {
-            Remove-Item -recurse -force $remote_directory_path
+    elseif ( ($file_status -eq "added") -or ($file_status -eq "modified") )
+    {
+        $dummy = $copy_files.Add($file_path)
+    }
+    elseif ($file_status -eq "unversioned")
+    {
+        # check if it is a source file
+        if ( CopyRequired($file_path) )
+        {
+            $dummy = $unversioned_files.Add($file_path)
         } else {
-            write "Directory missing on the remote"
+            write "Ignoring $file_path"
         }
     }
+}
 
-    # now process files
-    foreach ($file in $report.FileToCopy + $report.FileUnversioned) {
-        $local_file_path = GetLocalPath( $file)
-        $remote_file_path = GetPathOnRemote($file)
+#save current changes (merged changes will be restoring)
+$xml_settings = new-object System.Xml.XmlWriterSettings
 
-        write "Copy file: $remote_file_path"
-        Copy-Item -Force $local_file_path $remote_file_path
+$xml_settings.Indent = $True
+$xml_settings.IndentChars = "`t"
+
+$writer = [System.Xml.XmlWriter]::Create($report_file, $xml_settings)
+
+$writer.WriteStartDocument()
+
+$writer.WriteStartElement("Files")
+
+
+WriteFilesToXMLWriter $writer "ToCopy" $copy_files
+
+WriteFilesToXMLWriter $writer "ToDelete" $delete_files
+
+WriteFilesToXMLWriter $writer "Unversioned" $unversioned_files
+
+$writer.WriteEndElement() #files
+
+$writer.WriteEndDocument()
+$writer.Flush()
+$writer.Close()
+
+#merge lists
+foreach ($file in $old_unversioned_files) {
+    if ( ! $unversioned_files.Contains($file) ) {
+        if ( ! ( $copy_files.Contains($file) -or $delete_files.Contains($file) ) ) {
+            #unversioned file is not in there any more
+            $dummy = $delete_files.Add($file)
+        }
+    }
+}
+
+foreach ($file in $old_copied_files) {
+    if ( ! ( $copy_files.Contains($file) -or $unversioned_files.Contains($file) ) )
+    {
+        #file need to be restored
+        $dummy = $copy_files.Add($file)
+    }
+}
+
+foreach ($file in $old_deleted_files) {
+    if ( ! $delete_files.Contains( $file) ) {
+        #file need to be restored
+        if ( ! ( $copy_files.Contains($file) -or $delete_files.Contains($file) ) )
+        {
+            $local_file_path = $local_src_path + $file
+            if (Test-Path $local_file_path) {
+                $dummy = $copy_files.Add($file)
+            } else {
+                $dummy = $delete_files.Add($file)
+            }
+        }
+    }
+}
+
+$haveWork = ($copy_files.count -ne 0) -or ($unversioned_files.count -ne 0) -or ($delete_files.count -ne 0)
+
+if ($haveWork)
+{
+    #perfom copy
+    foreach ($file in $copy_files + $unversioned_files) {
+        $local_file_path = $local_src_path + $file
+        $remote_file_path = $remote_path + $file
+
+    write "Copy: $local_file_path"
+        if ( test-path $local_file_path -PathType Container) {
+            if ( test-path $remote_file_path) {
+                Remove-Item -recurse -force $remote_file_path
+            }
+
+            Copy-item -recurse -Force $local_file_path $remote_file_path
+        } else {
+            Copy-Item -Force $local_file_path $remote_file_path
+        }
+
     }
 
-    foreach ($file in $report.FileToDelete) {
-        $remote_file_path = GetPathOnRemote($file)
+    foreach ($file in $delete_files) {
+        $remote_file_path = $remote_path + $file
 
-        write "Delete file: $remote_file_path"
+        write "Delete: $remote_file_path"
         if (test-path $remote_file_path) {
-            Remove-Item $remote_file_path
-        } else {
-            write "File is missing on the remote"
+            if ( test-path $remote_file_path -PathType Container ) {
+                Remove-Item -recurse -force $remote_file_path
+            } else {
+                Remove-Item $remote_file_path
+            }
         }
     }
 } else {
